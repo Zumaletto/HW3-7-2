e8233806fdbd3fc1b09c31617eb58782bc1d2992
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-31 12:49:44 +0300

    added modified maven.yml and statistics

--- src/main/java/ru/netology/statistisc/StatisticsService.java
+++ src/main/java/ru/netology/statistisc/StatisticsService.java
@@ -1,2 +1,13 @@
-package ru.netology.statistisc;public class StatisticsService {
+package ru.netology.statistisc;
+
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+    }
 }
+
